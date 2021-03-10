9188e31020c770369f361c5537f5c6abdc825e0d
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-03-10 16:17:48 -0600

    added button for new post

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TransmissionListView, TransmissionDetailView
+from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView
 
 
 urlpatterns = [
     path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
     path('', TransmissionListView.as_view(), name='home'),
+    path('transmission/new/', TransmissionCreateView.as_view(), name='transmission_new'),
 ]
