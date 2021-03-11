5389a394088059e386f559520a2e017ca865c065
Andy Duarte <andyduarte@gmail.com>
2021-03-11 10:18:54 -0600

    pull

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView
+from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 
 
 urlpatterns = [
-    path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
     path('new/', CommentCreateView.as_view(), name='transmission_new'),
+    path('<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
+    path('<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('', TransmissionListView.as_view(), name='home'),
-    path('transmission/new/', TransmissionCreateView.as_view(), name='transmission_new'),
+    path('transmission/new/', TransmissionCreateView.as_view(), name='comment_new'),
 ]
--- transmissions/views.py
+++ transmissions/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Transmission, Comment
 from django.shortcuts import render
 from django.conf import settings
 from django.views.generic.edit import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.urls import reverse_lazy
 
 class TransmissionListView(ListView):
     model = Transmission
@@ -25,6 +26,15 @@ class TransmissionCreateView(LoginRequiredMixin, CreateView):
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
-    template_name = 'transmission_new.html'
+    template_name = 'comment_new.html'
     fields = ('transmission', 'comment', 'author')
 
+class CommentUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Comment
+    fields = ('comment')
+    template_name = 'comment_edit'
+
+class CommentDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Comment
+    template_name = 'comment_delete'
+    success_url = reverse_lazy('transmission_detail')
