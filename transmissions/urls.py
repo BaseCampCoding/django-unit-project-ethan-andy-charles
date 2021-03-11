4478389346567492839ba10d00d7e8bc1e654fa1
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-03-11 09:44:35 -0600

    Added Edit/Delete for Post

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView
+from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, TransmissionUpdateView, TransmissionDeleteView
 
 
 urlpatterns = [
+    path('Transmission/<int:pk>/delete/', TransmissionDeleteView.as_view(), name='transmission_delete'),
+    path('Transmission/<int:pk>/edit/', TransmissionUpdateView.as_view(), name='transmission_edit'),
     path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
     path('', TransmissionListView.as_view(), name='home'),
     path('transmission/new/', TransmissionCreateView.as_view(), name='transmission_new'),
--- transmissions/views.py
+++ transmissions/views.py
@@ -2,8 +2,8 @@ from django.views.generic import ListView, DetailView
 from .models import Transmission
 from django.shortcuts import render
 from django.conf import settings
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class TransmissionListView(ListView):
     model = Transmission
@@ -17,3 +17,13 @@ class TransmissionCreateView(CreateView):
     model = Transmission
     template_name = 'transmission_new.html'
     fields = ['title', 'author', 'body']
+
+class TransmissionUpdateView(UpdateView):
+    model = Transmission
+    template_name = 'transmission_edit.html'
+    fields = ['title', 'body']
+
+class TransmissionDeleteView(DeleteView):
+    model = Transmission
+    template_name = 'transmission_delete.html'
+    success_url =  reverse_lazy('home')
