86933322d10f75e11b58c47e5f010092f31d4cf0
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-03-11 10:09:31 -0600

    Added auth for edit and delete on post

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, TransmissionUpdateView, TransmissionDeleteView
+from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, TransmissionUpdateView, TransmissionDeleteView, CommentCreateView
 
 
 
--- transmissions/views.py
+++ transmissions/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class TransmissionListView(ListView):
     model = Transmission
@@ -21,18 +21,26 @@ class TransmissionCreateView(LoginRequiredMixin, CreateView):
     template_name = 'transmission_new.html'
     fields = ['title', 'author', 'body']
 
-class TransmissionUpdateView(UpdateView):
+class TransmissionUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Transmission
     template_name = 'transmission_edit.html'
     fields = ['title', 'body']
 
-class TransmissionDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class TransmissionDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Transmission
     template_name = 'transmission_delete.html'
     success_url =  reverse_lazy('home')
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
