9d8b1616710e3c0fae224b7e466bcefe633dc552
Andy Duarte <andyduarte@gmail.com>
2021-03-11 10:21:42 -0600

    updated

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 
-from .views import CommentCreateView
-from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, TransmissionUpdateView, TransmissionDeleteView
+from .views import TransmissionListView, TransmissionDetailView, TransmissionCreateView, CommentCreateView
 
 
 
 urlpatterns = [
     path('Transmission/<int:pk>/delete/', TransmissionDeleteView.as_view(), name='transmission_delete'),
     path('Transmission/<int:pk>/edit/', TransmissionUpdateView.as_view(), name='transmission_edit'),
-    path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
+    path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),m
     path('new/', CommentCreateView.as_view(), name='transmission_new'),
     path('<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
     path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
     path('<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('', TransmissionListView.as_view(), name='home'),
     path('transmission/new/', TransmissionCreateView.as_view(), name='comment_new'),
+
 ]
