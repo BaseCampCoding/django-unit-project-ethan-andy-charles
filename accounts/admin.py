4d6b615928d00214f2323f8b4018a65726440c96
Andy Duarte <andyduarte@gmail.com>
2021-03-12 09:00:11 -0600

    comment fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,10 +10,10 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', 'username', 'age', 'is_staff',]
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('age', )}),
+        (None, {'fields': ('age',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('age', 'email')}),
+        (None, {'fields': ('age',)}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
