9d81368a56f179186db4b5f27dc292857a7f5b90
Andy Duarte <andyduarte@gmail.com>
2021-03-16 14:58:39 -0500

    added pedit profile page and authentication.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse_lazy, reverse
 # Create your models here.
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField(max_length=255, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy('user_profile', args=[str(self.id)])
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUpView, ShowProfilePageView
+from .views import SignUpView, ShowProfilePageView, EditProfilePageView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('<int:pk>/', ShowProfilePageView.as_view(), name='user_profile')
+    path('<int:pk>/', ShowProfilePageView.as_view(), name='user_profile'),
+    path('<int:pk>/edit/', EditProfilePageView.as_view(), name='edit_profile')
 ]
+
