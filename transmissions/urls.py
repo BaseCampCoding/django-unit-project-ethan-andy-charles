1d9fefe5dc91855c4074a53a34b4bc46339d6a4a
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-03-17 10:27:35 -0500

    fixed duplicate for comments

--- transmissions/urls.py
+++ transmissions/urls.py
@@ -5,13 +5,15 @@ from .views import TransmissionListView, TransmissionDetailView, TransmissionCre
 
 
 urlpatterns = [
+    
     path('Transmission/<int:pk>/delete/', TransmissionDeleteView.as_view(), name='transmission_delete'),
     path('Transmission/<int:pk>/edit/', TransmissionUpdateView.as_view(), name='transmission_edit'),
     path('Transmission/<int:pk>/', TransmissionDetailView.as_view(), name='transmission_detail'),
     path('', TransmissionListView.as_view(), name='home'),
     path('transmission/new/', TransmissionCreateView.as_view(), name='transmission_new'),
     path('new/', CommentCreateView.as_view(), name='comment_new'),
-    path("<int:pk>/edit/", CommentUpdateView.as_view(), name='comment_edit'),
+    
     path("<int:pk>/delete/", CommentDeleteView.as_view(), name='comment_delete'),
-    path('like/<int:pk>', LikeView, name="like_transmission"), 
+    path('like/<int:pk>', LikeView, name="like_transmission"),
+    path("<int:pk>/edit/", CommentUpdateView.as_view(), name='comment_edit'),
 ]
