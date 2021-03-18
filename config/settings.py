c7c61c220f32b2a1309fb8236728efe23749895e
ecbishop1055 <ethancbishop1@gmail.com>
2021-03-18 09:41:12 -0500

    redis host change

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG' : {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('redis-rigid-57947', 6379)],
         },
     },
 }
